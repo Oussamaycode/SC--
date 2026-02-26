90be88805265d6609f752cafb4397a79bae4a8d0
Oussamaycode <oussamalamlouki047@gmail.com>
2026-02-26 12:59:04 +0000

    fix: streamline validation rules in StoreexpenseRequest for clarity

--- app/app/Http/Requests/StoreexpenseRequest.php
+++ app/app/Http/Requests/StoreexpenseRequest.php
@@ -23,8 +23,8 @@ class StoreexpenseRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            
+        return ['amount'=>['required','integer','min:0'],]
+
         ];
     }
 }
